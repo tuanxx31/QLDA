27a6be4a3ceff2f50190dd80a12603875c292437
tuanxx31 <225748020110138@vinhuni.edu.vn>
2025-10-31 10:16:07 +0700

    ok

--- qlda-fe/src/layouts/AuthLayout.tsx
+++ qlda-fe/src/layouts/AuthLayout.tsx
@@ -1,7 +1,18 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigate } from "react-router-dom";
 import "@/styles/auth.css";
+import useAuth from "@/hooks/useAuth";
+import { useEffect } from "react";
 
 export default function AuthLayout() {
+  const navigate = useNavigate()
+  const auth = useAuth()
+
+  useEffect(()=>{
+    if (auth.isAuthenticated) {
+      navigate("/")
+    }
+  },[])
+
   return (
     <div className="auth-layout">
       <Outlet />
