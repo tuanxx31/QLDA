7eca35c74c2ed12d057e892ef2a9bdbc6790689d
tuanxx31 <nguyendinhtuanx31@gmail.com>
2026-01-03 21:56:03 +0700

    feat: Add initial Docker Compose configurations for local and production environments, including frontend API base URL setup.

--- qlda-fe/src/utils/constants.ts
+++ qlda-fe/src/utils/constants.ts
@@ -1,2 +1,8 @@
 // API Base URL - có thể override bằng biến môi trường VITE_API_BASE
+
+let baseApi = import.meta.env.VITE_API_BASE 
+console.log({baseApi});
+
+// process.env.NODE_ENV === "production" && (baseApi = "https://api-server-mail.sclould.com");
+
 export const API_BASE = import.meta.env.VITE_API_BASE || 'http://localhost:3000/api';
